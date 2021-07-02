051054d9ceb7a50e9402c9ee592779461ab51907
EC2 Default User <ec2-user@ip-172-31-76-233.ec2.internal>
2021-07-02 14:47:48 +0000

    for each break

---
+++ amouth.rb
@@ -0,0 +1,4 @@
+amounth = { "リンゴ"=>2, "イチゴ"=>5, "ミカン"=>1 }
+amounth.each do |fruit, amounth|
+  puts "#{fruit}は#{amounth}個です"
+end
---
+++ break.rb
@@ -0,0 +1,10 @@
+i = 1
+while i <= 10 do
+  if i == 5
+    puts "処理を終了します"
+    break #5になると繰り返しから
+  end
+
+puts i
+i += 1
+end
